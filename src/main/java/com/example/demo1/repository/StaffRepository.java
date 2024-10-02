a9f27d3a8d0325341ac676b625c99d4bc85c480e
Lalit1150 <117420590+Lalit1150@users.noreply.github.com>
2024-10-03 00:02:09 +0530

    Delete src/main/java/com/example/demo1/repository/StaffRepository.java

--- src/main/java/com/example/demo1/repository/StaffRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo1.repository;
-
-import com.example.demo1.model.Staff;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StaffRepository extends JpaRepository<Staff, Integer> {
-
-}
