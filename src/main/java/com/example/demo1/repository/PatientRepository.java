c788c6f9bae5da9715c0c64fa181361b211722a8
Lalit1150 <117420590+Lalit1150@users.noreply.github.com>
2024-10-03 00:02:23 +0530

    Delete src/main/java/com/example/demo1/repository/PatientRepository.java

--- src/main/java/com/example/demo1/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo1.repository;
-
-import com.example.demo1.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-}
